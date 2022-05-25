68c62df7db931e329e4d48941c588c2494f6a1a8
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-25 19:23:34 +0530

    Welcome to the TicTacToe Game

---
+++ src/com/Bridgelabz/TicTacToe.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz;
+
+public class TicTacToe {
+    public static void board() {
+        char[] arr = new char[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = ' ';
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        board();
+    }
+}
+
